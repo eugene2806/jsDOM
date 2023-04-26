966ca8b9111bfffdbb1b8894f578cb6305994092
Eugene <smilefox2806@gmail.com>
2023-04-26 22:26:13 +0300

    DOM

---
+++ app.js
@@ -0,0 +1,5 @@
+'use strict'
+// const panelText = document.querySelector('.panel').innerText
+// console.log(panelText)
+// document.querySelector('.panel').innerText = 'New Text'
+// document.querySelector('.input').value = 'text'
