68a795f9a66883f83c8e8d0d3d26c0ef4a2d52db
Eugene <smilefox2806@gmail.com>
2023-04-27 11:30:01 +0300

    DOM2

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 'use strict'
-// const panelText = document.querySelector('.panel').innerText
-// console.log(panelText)
-// document.querySelector('.panel').innerText = 'New Text'
-// document.querySelector('.input').value = 'text'
+const panelText = document.querySelector('.panel').innerText
+console.log(panelText)
+document.querySelector('.panel').innerText = 'New Text'
+document.querySelector('.input').value = 'text'
+
+document.querySelector('.button').addEventListener('click', function(){
+    console.log('click')
+})  
