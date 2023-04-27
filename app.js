2d77c319ba0dd276a6ec5dd84e0577f87db4abf6
Eugene <smilefox2806@gmail.com>
2023-04-27 12:56:24 +0300

    Обработка нажатий

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 'use strict'
-const panelText = document.querySelector('.panel').innerText
-console.log(panelText)
-document.querySelector('.panel').innerText = 'New Text'
-document.querySelector('.input').value = 'text'
 
-document.querySelector('.button').addEventListener('click', function(){
-    console.log('click')
-})  
+// document.querySelector('.button').addEventListener('click', function() {
+//     const input = document.querySelector('.input').value
+//     if (!input) {
+//         return
+//     }
+//     document.querySelector('.panel').innerText = input
+//     document.querySelector('.input').value = ''
+// })
+
+function changeClick() {
+    const input = document.querySelector('.input').value
+    if (!input) {
+        return
+    }
+    document.querySelector('.panel').innerText = input
+    document.querySelector('.input').value = ''
+}
