facfc394292ec8e87646b387bc1f51ae3b253f7c
Eugene <smilefox2806@gmail.com>
2023-05-03 14:13:44 +0300

    Добавление HTML на лету

--- app.js
+++ app.js
@@ -9,21 +9,6 @@ function submitForm() {
     document.querySelector('.input').value = ''
     // document.querySelector('.notification').classList.add('notification_active')
     // document.querySelector('.notification').classList.remove('notification_hidden')
-//    console.log(document.querySelector('.notification').getAttribute('class'))
-   document.querySelector('.notification').setAttribute('class', 'notification')
-    document.querySelector('.notification').setAttribute('key', '1')
-    document.querySelector('.notification').setAttribute('user-id', '1')
-    // console.log(Number(document.querySelector('.notification').getAttribute('user-id')))
-    let elements = document.querySelectorAll('.one')
-    elements.forEach((el) => {
-        console.log(el.innerText)
-    })
-    // for (let i = 0; i < elements.length; i++) {
-    //     console.log(elements[i].innerText)
-    // }
-    // console.log(document.querySelectorAll('.one').innerText)
-    console.log(document.querySelector('#two').innerText)
-    console.log(document.querySelector(" [user-id='4']").innerText)
 
 }
 
@@ -32,4 +17,13 @@ function inputChanged(e) {
         submitForm()
     }
 }
+
+const panelText = 'Панель'
+const panelClass = 'button'
+const newElement = document.createElement('div')
+newElement.setAttribute('user-id', 1)
+newElement.classList.add('panel')
+// newElement.innerText = 'Button'
+newElement.innerHTML = `<button class=${panelClass}>${panelText}</button>`
+document.querySelector('.test').appendChild(newElement)
 
